2f93b4a8795190d7e41d5edf09256561fb9cfc7e
kryban93 <kryban1993@gmail.com>
2022-03-20 08:38:13 +0100

    Changes styles to CSS

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -5,7 +5,7 @@ import { HEROES } from 'src/app/mocked-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.sass'],
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
