7223374840abe62b162b676dbd40bc9ca2177c8d
kryban93 <kryban1993@gmail.com>
2022-03-23 20:18:31 +0100

    Added observable hero service

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Hero } from '../hero';
 import { HEROES } from '../mocked-heroes';
 
@@ -8,7 +9,8 @@ import { HEROES } from '../mocked-heroes';
 export class HeroService {
   constructor() {}
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
 }
